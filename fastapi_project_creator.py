54fbb1ed3873e554f12b2affd459b4316d58212d
maxmattappillil <116893586+maxmattappillil@users.noreply.github.com>
2024-05-29 11:20:52 -0400

    Fixed issue with making __init__ file in routers folder

--- fastapi_project_creator.py
+++ fastapi_project_creator.py
@@ -1,18 +1,20 @@
 import os
 
+
 def create_fastapi_project(project_name):
     # Check if the project directory already exists
     if os.path.exists(project_name):
-        print(f"The project directory '{project_name}' already exists. Please choose a different name.")
+        print(
+            f"The project directory '{project_name}' already exists. Please choose a different name.")
         return
-    
+
     # Define the project structure
     directories = [
         project_name,
         os.path.join(project_name, 'app'),
         os.path.join(project_name, 'app', 'routers'),
     ]
-    
+
     files = [
         os.path.join(project_name, 'app', '__init__.py'),
         os.path.join(project_name, 'app', 'main.py'),
@@ -22,22 +24,23 @@ def create_fastapi_project(project_name):
         os.path.join(project_name, 'app', 'config.py'),
         os.path.join(project_name, 'app', 'oauth2.py'),
         os.path.join(project_name, 'app', 'utils.py'),
-        os.path.join(project_name, 'routers', '__init__.py'),
+        os.path.join(project_name, 'app', 'routers', '__init__.py'),
         os.path.join(project_name, '.dockerignore'),
         os.path.join(project_name, 'DockerFile'),
         os.path.join(project_name, 'docker-compose.yml'),
         os.path.join(project_name, '.env'),
     ]
-    
+
     # Create directories
     for directory in directories:
         os.makedirs(directory, exist_ok=True)
-    
+
     # Create files
     for file in files:
         open(file, 'w').close()
-    
+
     print(f"Project structure for '{project_name}' has been created.")
+
 
 if __name__ == '__main__':
     project_name = input("Enter the project name: ")
